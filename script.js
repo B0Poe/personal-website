0e72897fb4ccfc74771cac87739a995c5592477e
B0Poe <umackirac01@gmail.com>
2025-01-15 13:13:18 +0300

    we making progress

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-// Get all the navigation links
 const navLinks = document.querySelectorAll('.topnav a');
 
-// Add click event listener to each link
 navLinks.forEach(link => {
     link.addEventListener('click', function() {
-        // Remove 'active' class from all links
         navLinks.forEach(nav => nav.classList.remove('active'));
         
-        // Add 'active' class to the clicked link
         this.classList.add('active');
     });
 });
